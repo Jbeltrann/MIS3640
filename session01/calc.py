74e4942f62dba2e0a9a6e8492adace485fe89707
Jbeltrann <jbeltran1@babson.edu>
2018-09-05 20:56:09 -0400

    Hw 2 for calc.py

--- session01/calc.py
+++ session01/calc.py
@@ -1,5 +1,19 @@
-print('8 + 30 + 2018')
+print ('8 + 30 + 2018')
 print ( ' Question 1 ' ) , print ( 42 * 60 + 42 )
 print ( ' Question 2 ') , print ( 10 / 1.61 )
 print ( ' Question 3 part 1 ' ) , print ( ( 10 / 1.61 ) / 42.7 )
 print ( ' Question 3 part 2 ' ) , print ( ( 10 / 1.61 ) / ( 42.7 / 60 ) )
+
+import math
+math.pi
+print ('Homework 2')
+print ('Question 1')
+print ('The volume of a sphere with a radius of 5 is V =', 4 / 3* math.pi * 5 ** 3)
+print ('Question 2')
+q2 = 24.95 * .6 + 60 + 3 + 0.75 * 59 
+print ('The total wholesale cost for 60 copies of the book would be{:.2f}'.format(q2))
+print ('Question 3')
+print ('After your morning run, you will get home for breakfast at', 8.25 + 7.2 * 3 / 60 + 6.52)
+print ('Question 4')
+q4 = 7/ 82 * 100
+print ('The percent of the increase of your grade would be {:04.2f}%'.format(q4))
---
+++ session02/demo-02.py
@@ -0,0 +1,31 @@
+print ('The sum is', 2 +  2 + 2 + 2)
+
+name = input(
+
+print ( 'Hello,' , name)
+
+name = input( ' Please enter your name' )   
+
+message = 'I learned something cool today'
+n = 100
+pi =3.14
+
+
+a = 123
+print (a)
+a = 'ABC'
+print (a)
+
+first_name = 'Jonathan'
+last_name = 'Beltran'
+name = first_name + ' ' + last_name
+
+print ( 'Hello, {}' .format(name))
+
+print('Congratulations, {:s}, you won {:d}in academy award' .format(name, 90))
+
+print ('P1 equals {:010.2f}'.format(3.14159265))
+
+print ('Growth rate: {:2f}%'.format(2.5))
+
+print ('Coordinates of Babson: {lat}, {lon}'.format(lon='71.27W', lat='42.30N'))
