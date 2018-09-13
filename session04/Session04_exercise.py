9a19046414d8269551721aecaca7af449d386295
Jbeltrann <jbeltran1@babson.edu>
2018-09-13 15:06:13 -0400

    Session4 Excercise

--- session04/Session04_exercise.py
+++ session04/Session04_exercise.py
@@ -2,10 +2,10 @@ import math
 
 def quadratic(a, b, c):
     d = b**2 - 4*a*c
-    if d <0:
-        print ("No solution")
-    else d >= 0:
-     x = (-b+math.sqrt(b**2-4*a*c))/2*a 
-     x1 = (-b-math.sqrt(b**2-4*a*c))/2*a
-    print ("X equals: ", x, " and", x1)    
-quadratic(1, 2, 3)
+    if d >= 0:
+        x = (-b+math.sqrt(b**2-4*a*c))/2*a 
+        x1 = (-b-math.sqrt(b**2-4*a*c))/2*a
+        print("X equals: ", x, " and", x1) 
+    else:
+        print("Does not have an answer")    
+quadratic(5,9,-11)
